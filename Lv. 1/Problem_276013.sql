8642286332e0a5f56da137cad63347368a37c7dc
SeungHyeon <45845547+namsh1125@users.noreply.github.com>
2024-09-13 21:53:20 +0900

    Solve Problem

---
+++ Lv. 1/Problem_276013.sql
@@ -0,0 +1,7 @@
+-- 프로그래머스 276013번. Python 개발자 찾기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/276013
+
+SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
+FROM DEVELOPER_INFOS
+WHERE SKILL_1 = 'Python' OR SKILL_2 = 'Python' OR SKILL_3 = 'Python'
+ORDER BY ID;
