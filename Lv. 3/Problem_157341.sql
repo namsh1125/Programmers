5c600e482c1460f2e4cb73c1f448e26051d31e9b
SeungHyeon <45845547+namsh1125@users.noreply.github.com>
2024-10-03 23:19:07 +0900

    Solve Problem

---
+++ Lv. 3/Problem_157341.sql
@@ -0,0 +1,9 @@
+-- 프로그래머스 157341번. 대여 기록이 존재하는 자동차 리스트 구하기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/157341
+
+SELECT DISTINCT A.CAR_ID
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY AS A
+JOIN CAR_RENTAL_COMPANY_CAR AS B
+ON A.CAR_ID = B.CAR_ID
+WHERE MONTH (A.START_DATE) = 10 AND B.CAR_TYPE = '세단'
+ORDER BY CAR_ID DESC
