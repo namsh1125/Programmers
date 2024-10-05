64dd7fdeeb1ca4f96e8d9fadd3081fe5bbfe1e4d
SeungHyeon <45845547+namsh1125@users.noreply.github.com>
2024-10-05 11:23:42 +0900

    Solve Problem

---
+++ Lv. 3/Problem_59043.sql
@@ -0,0 +1,9 @@
+-- 프로그래머스 59043번. 있었는데요 없었습니다
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59043
+
+SELECT I.ANIMAL_ID, I.NAME
+FROM ANIMAL_INS AS I
+JOIN ANIMAL_OUTS AS O
+ON I.ANIMAL_ID = O.ANIMAL_ID
+WHERE I.DATETIME > O.DATETIME
+ORDER BY I.DATETIME;
