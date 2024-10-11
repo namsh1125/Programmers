8998494caa847f22112cdb11d1af8bc4292281f4
SeungHyeon <45845547+namsh1125@users.noreply.github.com>
2024-10-11 17:00:20 +0900

    Solve Problem

---
+++ Lv. 4/Problem_59045.sql
@@ -0,0 +1,10 @@
+-- 프로그래머스 59045번. 보호소에서 중성화한 동물
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59045
+
+SELECT I.ANIMAL_ID, I.ANIMAL_TYPE, I.NAME
+FROM ANIMAL_INS AS I
+JOIN ANIMAL_OUTS AS O
+ON I.ANIMAL_ID = O.ANIMAL_ID
+WHERE I.SEX_UPON_INTAKE LIKE '%Intact%'
+    AND (O.SEX_UPON_OUTCOME LIKE '%Spayed%' OR O.SEX_UPON_OUTCOME LIKE '%Neutered%')
+ORDER BY I.ANIMAL_ID;
