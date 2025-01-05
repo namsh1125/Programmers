b9b7729e1d0b6ee296be9604f80ee9be6423ce85
SeungHyeon <45845547+namsh1125@users.noreply.github.com>
2025-01-05 23:42:29 +0900

    Solve Problem

---
+++ Lv. 1/Problem_12937.java
@@ -0,0 +1,10 @@
+/*
+ * 프로그래머스 12937번. 짝수와 홀수
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12937
+ */
+
+class Solution {
+    public String solution(int num) {
+        return num % 2 == 0 ? "Even" : "Odd";
+    }
+}
